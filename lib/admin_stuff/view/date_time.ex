af948d9cac3b1f24c2ba440f89eb71466a452e7b
Andrey Zaika <anzaika@gmail.com>
2019-10-29 12:57:46 +0400

    fix

--- lib/admin_stuff/view/date_time.ex
+++ lib/admin_stuff/view/date_time.ex
@@ -1,7 +1,7 @@
 defmodule AdminStuff.View.DateTime do
-  defmacro date_to_string(date, _opts \\ []) do
+  defmacro date_to_string(date, locale, _opts \\ []) do
     quote do
-      unquote(date) |> Cldr.Date.to_string(locale: @locale) |> elem(1)
+      unquote(date) |> Cldr.Date.to_string(locale: unquote(locale)) |> elem(1)
     end
   end
 end
