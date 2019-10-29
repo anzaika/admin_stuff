016fc95ad5e0561b6aaa90749bd52661ba10ae1e
Andrey Zaika <anzaika@gmail.com>
2019-10-29 13:01:30 +0400

    add datetime_to_string

--- lib/admin_stuff/view/date_time.ex
+++ lib/admin_stuff/view/date_time.ex
@@ -4,4 +4,10 @@ defmodule AdminStuff.View.DateTime do
       unquote(date) |> Cldr.Date.to_string(locale: unquote(locale)) |> elem(1)
     end
   end
+
+  defmacro datetime_to_string(date, locale, _opts \\ []) do
+    quote do
+      unquote(date) |> Cldr.DateTime.to_string(locale: unquote(locale)) |> elem(1)
+    end
+  end
 end
