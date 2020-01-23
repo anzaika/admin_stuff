4360b36599fa19871d1b74330149823dbe8cd843
Andrey Zaika <anzaika@gmail.com>
2020-01-23 15:50:20 +0300

    Update to mount/3
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- lib/admin_stuff/live_view/general.ex
+++ lib/admin_stuff/live_view/general.ex
@@ -1,12 +1,12 @@
 defmodule AdminStuff.LiveView.General do
   defmacro mounts(function) do
     quote do
-      def mount(%{locale: locale} = assigns, socket) do
+      def mount(_params, %{locale: locale} = assigns, socket) do
         Gettext.put_locale(locale)
         {:ok, apply(unquote(function), [assign(socket, assigns)])}
       end
 
-      def mount(assigns, socket) do
+      def mount(_params, assigns, socket) do
         assigns = Map.put_new_lazy(assigns, :locale, &Gettext.get_locale/0)
         {:ok, apply(unquote(function), [assign(socket, assigns)])}
       end
